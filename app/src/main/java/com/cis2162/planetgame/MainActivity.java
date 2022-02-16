2836198603eb24764cd1f85624a1379db13be0e1
Taylor Byrne <taylorbyrne@Taylors-MacBook-Pro.local>
2022-02-16 10:51:19 +0000

    Initial commit

---
+++ app/src/main/java/com/cis2162/planetgame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cis2162.planetgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
